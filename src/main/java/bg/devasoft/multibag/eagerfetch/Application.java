59b8f587b05d261c257af3202a6bd1a8788a0fca
Ivo <ivailoss@gmail.com>
2023-07-14 00:09:44 +0300

    Initial commit

---
+++ src/main/java/bg/devasoft/multibag/eagerfetch/Application.java
@@ -0,0 +1,13 @@
+package bg.devasoft.multibag.eagerfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
